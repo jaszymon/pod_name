3c9d73dbbeb328f1cb97008b9e85abe3d58ad61e
Szymon Jakubiak <szymon.jakubiak@inetum.world>
2021-04-27 11:10:58 +0200

    added api support to website

---
+++ public-html/script.js
@@ -0,0 +1,12 @@
+let request = new XMLHttpRequest();
+
+request.open("GET","http://20.56.237.3:8081/api/v1/pods");
+request.send();
+request.onload = () => {
+	console.log(request);
+	if (request.status ==200) {
+	console.log(JSON.parse(request.response));
+	} else {
+		console.log (`error ${request.status} ${request.statusText}`)
+	}
+}
